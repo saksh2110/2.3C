9d3e240d004b2fce822a837dfe5b0904cbf35da3
saksh2110 <139182976+saksh2110@users.noreply.github.com>
2023-09-25 16:15:47 +1000

    HomeView.vue

--- HomeView.vue
+++ HomeView.vue
@@ -1,22 +1,27 @@
 <template>
   <div class="math-tuition">
+    <!-- Header -->
     <h2>Math Tuition Details</h2>
     <p>Course: Algebra Basics</p>
     <p>Teacher: Mr. Saksham Nagpal</p>
     <p>Class Schedule: Mondays and Wednesdays, 5:00 PM - 7:00 PM</p>
 
+    <!-- Course Information -->
     <h2>Course Information</h2>
     <p v-html="courseDescription"></p>
 
+    <!-- Student List -->
     <h2>Student List</h2>
     <ul>
       <li v-for="student in students" :key="student.id">{{ student.name }}</li>
     </ul>
 
+    <!-- Enroll in the Course Form -->
     <h2>Enroll in the Course</h2>
     <label for="studentName">Your Name:</label>
     <input type="text" v-model="studentName" />
 
+    <!-- Enroll Button -->
     <button @click="enroll">Enroll Now</button>
   </div>
 </template>
@@ -24,14 +29,20 @@
 <script setup>
 import { ref } from 'vue';
 
+// Course description as HTML
 const courseDescription = '<p>Learn the basics of algebra in this comprehensive course. Improve your math skills and ace your exams!</p>';
+
+// Student name input field
 const studentName = ref('');
+
+// List of students
 const students = ref([
   { id: 1, name: 'Krish' },
   { id: 2, name: 'Dhruv' },
   { id: 3, name: 'Shivansh' },
 ]);
 
+// Function to enroll a student
 const enroll = () => {
   if (studentName.value) {
     students.value.push({ id: students.value.length + 1, name: studentName.value });
